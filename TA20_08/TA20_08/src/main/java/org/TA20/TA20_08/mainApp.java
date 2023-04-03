58a37fef03f6fbee7e8efba4c0d5adf9d8b01802
Francisco Parra <franparpe@disroot.org>
2023-04-03 19:19:47 +0200

    Primer commit ejercicio 8

---
+++ TA20_08/TA20_08/src/main/java/org/TA20/TA20_08/mainApp.java
@@ -0,0 +1,9 @@
+package org.TA20.TA20_08;
+
+import gui_elements.MyMainWindow;
+
+public class mainApp {
+	public static void main(String[] args) {
+		MyMainWindow my_window = new MyMainWindow();
+	}
+}
